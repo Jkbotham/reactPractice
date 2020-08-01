da03a6d3268a844702ce7a49d9ef5b8f300e956f
James <jamesbotham@gmail.com>
2020-08-01 15:44:46 -0500

    New api for handling client location and creating weatherNow component

---
+++ client/src/components/pages/weatherApp/weatherProps/now.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../weather.css";
+
+function Now (apiResponse) {
+    
+    return (
+        <div className="footer-margin">
+            <footer className="card-footer ooter-font-style text-#ffffff footer-position"> 2020 - Botham Practice Inc. ©</footer>
+        </div>
+    )
+}
+
+export default Now;
