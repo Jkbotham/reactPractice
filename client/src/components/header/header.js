9f2df1c1b333e43daa543f47f19458b1a2a03763
James <jamesbotham@gmail.com>
2020-08-14 21:48:50 -0500

    Updated Homepage to include easier link to weather project

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-// import "./header.css"
+import "./header.css";
 
 function Header() {
   return (
@@ -10,15 +10,15 @@ function Header() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          {/* <Nav.Link href="/about-us">Contact Us</Nav.Link>
-        <Nav.Link href="/contact-us">About Us</Nav.Link> */}
-          <NavDropdown title="" id="nav-dropdown-align-right">
-            <NavDropdown.Item href="/weatherApp">WeatherApp</NavDropdown.Item>
-            {/* <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <Nav.Link href="/weatherApp">Weather App</Nav.Link>
+          {/* <Nav.Link href="/contact-us">About Us</Nav.Link> 
+       <NavDropdown title="" id="nav-dropdown-align-right">
+            <NavDropdown.Item href="/weatherApp">WeatherApp</NavDropdown.Item> 
+             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-          </NavDropdown>
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> 
+         </NavDropdown> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- client/src/components/pages/mainPage.js
+++ client/src/components/pages/mainPage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Jumbotron, Card, Image } from "react-bootstrap";
 import "./pages.css";
 import Header from "../header/header";
+import imgWeatherApp from "../../assets/images/weatherApp.png";
 
 function mainPage() {
   return (
@@ -10,7 +11,37 @@ function mainPage() {
       <Container>
         <Row>
           <Col>
-            <h1> Main Page </h1>
+            <h1> Home Page </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <Row>
+                <Col>
+                  <Card>
+                    <Card.Link href="/weatherApp">
+                      <Card.Img
+                        variant="bottom"
+                        className="mainPageImg"
+                        src={imgWeatherApp}
+                      />
+                      <Card.Title>Weather App</Card.Title>
+                    </Card.Link>
+
+                    {/* <p>Weather App</p>
+                    <a href="/weatherApp" className="mainPageImgLink">
+                      <Image
+                        className="mainPageImg"
+                        alt="Screenshot of weather app page"
+                        src={imgWeatherApp}
+                        href="/weatherApp"
+                      ></Image>
+                    </a> */}
+                  </Card>
+                </Col>
+              </Row>
+            </Jumbotron>
           </Col>
         </Row>
       </Container>
